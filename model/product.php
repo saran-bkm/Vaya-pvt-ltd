cdcc67442e4a591d8528a6742cf7909cd33c1f78
saran-bkm <saranbkm@hotmail.com>
2025-12-10 16:04:41 +0530

    update completed

--- model/product.php
+++ model/product.php
@@ -51,13 +51,13 @@ switch ($action) {
                $data .="<td>".$d['qty']."</td>";
                $data .="<td>".$d['created_at']."</td>";
               $data .= '<td>
-            <button class="btn btn-sm btn-primary" onclick="editProduct(' . $d['id'] . ')">
-                <i class="fa-solid fa-pen-to-square"></i>
-            </button>
-            <button class="btn btn-sm btn-danger" onclick="deleteProduct(' . $d['id'] . ')">
-                <i class="fa-solid fa-trash"></i>
-            </button>
-          </td>';
+                <button class="btn btn-sm btn-primary" onclick="editProduct(' . $d['id'] . ')">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button class="btn btn-sm btn-danger" onclick="deleteProduct(' . $d['id'] . ')">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>';
 
                $data .="</tr>";
             }
